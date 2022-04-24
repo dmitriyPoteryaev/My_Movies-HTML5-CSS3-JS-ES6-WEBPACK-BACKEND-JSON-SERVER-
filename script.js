69d6582152f4b25c0a1b15249c081593dc85993b
dmitriyPoteryaev <dim.poteryaeff@yandex.ru>
2022-04-24 14:00:25 +0300

    First message

---
+++ script.js
@@ -0,0 +1,521 @@
+
+window.addEventListener('DOMContentLoaded', () =>{
+
+
+"use strict";
+
+const movieDB = {
+    movies: [
+      
+    ]
+};
+
+
+
+const ListOfFilms = document.querySelector(`.promo__interactive-list`);
+const NameOfFilm = ListOfFilms.querySelectorAll(`.promo__interactive-item`);
+
+
+
+
+const ButtonOfDelete = document.createElement(`button`);
+
+
+
+ButtonOfDelete.style.cssText = `
+ background-image: url("css/img/trash2.jpg"); background-repeat: no-repeat;
+ width: 30px; height: 40px; margin-left: 0px; margin-left: 10px; border: 0; `;
+
+
+
+// функция предназначена для удаления определённого фильма
+  (function DeleteFilmFronList(){
+ButtonOfDelete.addEventListener(`click`, () =>{
+    
+ 
+
+   const input_value=ButtonOfDelete.parentElement.innerHTML;
+let p =0;
+
+   for(let letter of input_value){
+
+if(p==0){
+  console.log(ButtonOfDelete.parentElement);
+  ButtonOfDelete.parentElement.remove();
+    movieDB.movies.splice(letter-1, 1);//данный метод удаляет определённый элемент массива
+     Main();//рекурсия
+   }
+    p++;
+   }
+});
+
+})()
+
+
+// слушатель события предназначен для того,чтобы показать кнопку удаления на конкретном фильме
+
+
+
+ 
+
+  ListOfFilms.addEventListener(`mouseout`,() =>{
+    
+    for(let node of  ListOfFilms.childNodes){
+
+      if(node.className=='promo__interactive-item'){
+      Main('nothing');
+   
+      }
+    }
+  
+  
+  });
+
+
+// пушим названия всех фильмов из HTML-структуры
+for(let item of  NameOfFilm){
+
+
+for(let node of  item.childNodes){
+
+    movieDB.movies.push(node.data);
+
+}
+
+}
+
+
+
+
+
+//вызываем функцию Main
+
+Main('nothing');
+
+
+
+
+
+
+
+ const buttonOfAdd =document.querySelector(`.accept`),
+ input = document.querySelector(`.adding__input`);
+
+
+
+// слушатель события предназначен для того,чтобы запушить новое название фильма в список
+
+ buttonOfAdd.addEventListener(`click`, (event) =>
+{
+  event.preventDefault();
+ 
+      if(input.value=='    '){
+
+      alert(`Пожалуйста,введите название фильма!`);
+        
+            }
+
+else{
+
+
+   push();
+    //НАЧАЛО ФУНКЦИИ
+   Main('nothing');
+
+           //КОНЕЦ ФУНКЦИИ
+          
+           }
+
+       });
+
+
+      //  пушим новое название фильма через инпут
+
+       function push(){
+
+
+
+        movieDB.movies.push(input.value.toUpperCase());
+        
+        }
+
+
+
+
+function Main (k){
+    ListOfFilms.innerHTML= '';
+    //cначала удаляем полностью весь список.Сначала il
+
+//здесь записываем с этот массив название фильм
+
+
+
+
+
+movieDB.movies.sort();// сортируем весь список фильмов в массиве
+
+// заполняем список фильмами
+
+
+
+
+movieDB.movies.forEach( (item,j) =>  {
+
+
+
+//функция ,предназначена для формирования нового списка фильмов на странице
+  function NewListOfFilms(){
+    const NameOfFilm = item;
+    let i =1;
+      
+    let NewNameOfFilm=' ';
+     
+    for(let letter of NameOfFilm){
+  
+        if(letter==` `||letter==`-`){
+  
+        
+         NewNameOfFilm = NewNameOfFilm + letter;
+         continue;
+        }
+  
+        else{
+        
+        NewNameOfFilm = NewNameOfFilm + letter;
+        
+        if(i==20){
+         NewNameOfFilm = NewNameOfFilm + `...`;
+      
+    break;
+    
+        }
+        
+      }
+        
+      i++;  
+    
+  }
+  
+  
+  return NewNameOfFilm
+  }
+
+
+
+    ListOfFilms.insertAdjacentHTML(`beforeend`, `<li class="promo__interactive-item">${j+1}.${NewListOfFilms()}
+    </li>`);
+
+    if(j===k){
+    const NameOfFilm = ListOfFilms.querySelectorAll(`.promo__interactive-item`);
+
+    NameOfFilm[j].insertAdjacentElement(`beforeend`, ButtonOfDelete);
+
+
+}
+
+
+});
+
+
+
+  const NameOfFilm = ListOfFilms.querySelectorAll(`.promo__interactive-item`);
+
+
+
+NameOfFilm.forEach( (item1,k) =>{
+
+
+    item1.addEventListener(`mouseenter`,(CopyMain));
+   
+
+    function CopyMain(){
+         
+        Main(k);
+       
+    }
+
+
+});
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//промо bg нужно очистить и вставить новое
+
+
+const Content = document.querySelector(`.promo__content`),
+backstage = Content.querySelectorAll(`.promo__bg`),
+ListOfNewFilm = document.querySelector(`.promo__adv`),
+NewFilmOnRight = ListOfNewFilm.querySelectorAll(`.new_film`),
+Interc =document.querySelector(`.promo__interactive`);
+
+ hideAllWorkSpace();
+showContent();
+
+
+// функция, предназначена для полной очистики всего рабочего поля
+function hideAllWorkSpace(){
+
+
+
+  // 1 функция
+function hideLeftFilm(){
+
+for(let node of Content.childNodes){
+ 
+ 
+  if(node.className=='rightFilm'){
+ node.remove();
+ }
+ }
+ }
+ hideLeftFilm();
+
+
+ // 2 функция
+ function hidePromoBg(){
+
+  backstage.forEach(item => {
+
+item.style.display='none';
+
+
+});
+
+
+  NewFilmOnRight.forEach(item1 => {
+
+    item1.classList.remove('new_film_active');
+
+      
+      });
+
+  
+
+
+
+}
+hidePromoBg();
+
+
+// 3 функция
+function hideIntrc(){
+
+
+  Interc.style.display="none";
+
+}
+
+hideIntrc();
+
+}
+
+
+
+// функция, предназначена для  отображения контента ,связанным с новыми фильмами 
+// и блоком ,который предназначен для отображения списка любымых фильмов
+function showContent(i=1,moveFilm='Человек-паук'){
+  Interc.style.display="flex";
+  backstage[i].style.display='block';
+  
+  Content.style.cssText = `background-image: url("css/img/${moveFilm}.jpg"); background-repeat: no-repeat;`
+  NewFilmOnRight[i].classList.add('new_film_active');
+}
+
+
+
+
+NewFilmOnRight.forEach((item1,i) => {
+  item1.addEventListener(`click`,() =>{
+ 
+  
+    hideAllWorkSpace()
+    
+    showContent(i,item1.innerHTML);
+
+      
+      
+      
+      });
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const LeftButton =document.querySelector(`.promo__menu-item_films `);
+
+
+
+//Даннный класс предназнчен для формирования списка фильмов
+
+class Block_Of_Films_and_Serials{
+
+
+
+push_new_film(text1,photoOfMovies){
+
+  this.text1=text1;
+  this.photoOfMovies=photoOfMovies;
+  Content.insertAdjacentHTML(`afterbegin`, `
+  
+  <div class="rightFilm" style=" display:flex;
+  flex-direction: column;  
+  justify-content: space-around;
+   width:300px; height:400px; margin:100px; font-family: 'Roboto', sans-serif;
+   font-weight: 800; font-size: 20px; text-align:center; text-align:bottom; ">
+
+  <div ><img src="forFilmOnLeft/${photoOfMovies}.jpg " width="90%" height="90%" ></div>
+  <div >${text1}</div>
+  
+   </div>
+  
+  
+  
+
+  `);
+}
+
+
+
+
+}
+// создаём новые объеты  под фильмы
+
+
+const Something_film =  new Block_Of_Films_and_Serials();
+
+
+
+  LeftButton.addEventListener(`click`,ForLeft)
+ 
+  
+ 
+
+function ForLeft(){
+
+  hideAllWorkSpace()
+
+  Content.style.cssText = ` flex-direction:row; flex-wrap:wrap;`
+  Something_film.push_new_film(`Побег из Шоушенка`,`Побег из Шоушенка`);
+  Something_film.push_new_film(`Властелин Колец: Две крепости`,`Властелин Колец(Две крепости)`);
+  Something_film.push_new_film(`Криминальное чтиво`,`Криминальное чтиво`);
+  Something_film.push_new_film(`Интерстеллар`,`Интерстеллар`);
+  Something_film.push_new_film(`Отступники`,`Отступники`);
+  Something_film.push_new_film(`Тёмный Рыцарь`,`Тёмный Рыцарь`);
+  Something_film.push_new_film(`Гладиатор`,`Гладиатор`);
+  Something_film.push_new_film(`Джанго освобождённый`,`Джанго освобождённый`);
+  Something_film.push_new_film(`Поймай меня, если сможешь`,`Поймай меня, если сможешь`);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
